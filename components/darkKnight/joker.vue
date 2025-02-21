36794eb735dc7854472ac76147d44cc7546152e1
laurent <laurentdoggy@gmail.com>
2025-02-21 11:05:17 +0100

    first commit

--- components/darkKnight/joker.vue
+++ components/darkKnight/joker.vue
@@ -7,7 +7,23 @@
         <div class="pb-10">
             <video class="rounded-lg  my-14 mx-auto" src="/images/bat origins/Lacey Towers Cutscene.mp4" controls="controls"></video>
         </div>
-        <img src="https://i.pinimg.com/736x/88/de/92/88de92262f6769031da439a4e076189b.jpg" alt="">
+        <div class="joke grid grid-cols-2 border-2  rounded-xl shadow-black shadow-xl 
+        bg-gradient-to-r from-emerald-600 to-purple-800 to-35%">
+            <img src="/images/bat origins/jk1.png" alt="" class="mag ml-20 ">
+            <div class="mr-20 pt-16 text-gray-300">
+                 <h2 class="text-center text-4xl pb-4 uppercase font-bold">the joker</h2>
+                <p class="pt-4 text-justify text-lg">Lors du soir de Noël, lorsque le Joker déguisé en Black Mask tente de déclencher une émeute, Batman entre dans la prison pour l'arrêter.
+    
+                Il tue le commissaire Gillian B. Loeb et quitte la prison. Plus tard, Batman découvre l'auteur du meurtre des Lacey Tower, qui n'est autre que le Joker. Il se dirige à la banque de Gotham City où le Joker se révèle. Batman le poursuit et l'arrête dans l'hôtel Royal.
+    
+                Dans celle-ci, il est attaché et parle avec une psychologue. Il lui explique qu'elle ne connait pas ce qu'est vraiment une mauvaise journée et explique des sensations. Pendant la discussion, le joueur incarne le Joker qui se bat contre des hommes. Il est ensuite habillé en Red Hood dans une usine chimique. Dans la discussion avec la psychologue, il parle d'une rencontre spéciale, sans doute Batman. Dans l'usine, Le Joker est projeté dans une cuve d'acide, faisant de sa couleur de peau, un blanc anormal. Elle lui demande s'il connait cette personne, ce à quoi il indique ne pas connaitre sa véritable identité. La psychologue se présente et dit s'appeler Harleen Quinzel. Il lui demande alors si ses amis l'appelle Harley mais n'ayant pas beaucoup d'amis, ce n'est pas le cas. Le Joker lui dit que maintenant elle en a un. Il semble évident qu'elle tombe amoureuse de lui mais que ce dernier la manipule.
+    
+                Finalement, il est libre lors de l'émeute de la prison de Blackgate, menace Gordon, tire sur Joseph et se prend une raclée par Batman. Il est remis en prison. </p>          
+            </div>
+        </div> 
+        <div class="pt-36 pb-10">
+            <img src="https://wallpapers.com/images/hd/batman-arkham-origins-wallpaper-ao09atleqceb6on5.jpg" alt="">
+        </div>      
     </div>
 </template>
 
@@ -16,6 +32,13 @@
 </script>
 
 <style scoped>
+    .joke{
+        width: 70%;
+        margin: 0 auto;
+    }
+    .mag{
+        width: 60%;
+    }
     .pic{
         width: 40%;
     }
--- components/darkKnight/justice.vue
+++ components/darkKnight/justice.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="board">
-        <div class="grid grid-cols-2">
-            <div>
-                <img src="/images/bat origins/bat style.png" alt="">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem dolor incidunt eius quae voluptas blanditiis hic commodi velit et culpa!</p>
-            </div>
-            <div>
-                <img src="/images/bat origins/alfred.png" alt="">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo, deleniti velit. Iusto ea facilis perferendis unde porro ullam laborum corrupti.</p>
+        <div class="grid grid-cols-2 pb-20">
+            <div v-for="mode in modes" class="m-4">
+                <img :src="mode.pic" alt="" class="rounded-lg shadow-xl  shadow-gray-800">
+                <div class="m-2 pt-2">
+                    <h2 class="uppercase font-extrabold">{{ mode.name }}</h2>
+                    <p class="capitalize">{{ mode.content }}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -15,11 +14,18 @@
 
 <script setup>
 
+    const modes = [
+        { pic:"https://e1.pxfuel.com/desktop-wallpaper/409/984/desktop-wallpaper-batman-arkham-knight-gotham-city.jpg", name:"bat signal", content:"le signal qui déchire le ciel."},
+        { pic:"https://static.psthc.fr/imgT/2_1436205695.jpg", name:"carte de gotham city", content:"parcourez la ville pour éliminer la menace."},
+        { pic:"https://i.ytimg.com/vi/zzWjKN66grc/maxresdefault.jpg", name:"costumes de batman", content:"débloquez-les en accomplissant des missions."},
+        { pic:"https://cdnb.artstation.com/p/assets/images/images/001/733/303/large/ronan-mahon-rmahongcpd-7.jpg?1451941261", name:"gotham city police départment", content:"le commissaire gordon vous traquera avec sa brigade."},
+    ]
+
 </script>
 
 <style scoped>
     .board{
-        width: 50%;
+        width: 60%;
         margin: 0 auto;
     }
     img{
