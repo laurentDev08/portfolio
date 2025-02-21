a589e66226d82d98815e976cf81392a4751671fc
laurent <laurentdoggy@gmail.com>
2025-02-21 13:08:01 +0100

    joker id

--- components/darkKnight/joker.vue
+++ components/darkKnight/joker.vue
@@ -11,14 +11,31 @@
         bg-gradient-to-r from-emerald-600 to-purple-800 to-35%">
             <img src="/images/bat origins/jk1.png" alt="" class="mag ml-20 ">
             <div class="mr-20 pt-16 text-gray-300">
-                 <h2 class="text-center text-4xl pb-4 uppercase font-bold">the joker</h2>
-                <p class="pt-4 text-justify text-lg">Lors du soir de Noël, lorsque le Joker déguisé en Black Mask tente de déclencher une émeute, Batman entre dans la prison pour l'arrêter.
-    
-                Il tue le commissaire Gillian B. Loeb et quitte la prison. Plus tard, Batman découvre l'auteur du meurtre des Lacey Tower, qui n'est autre que le Joker. Il se dirige à la banque de Gotham City où le Joker se révèle. Batman le poursuit et l'arrête dans l'hôtel Royal.
-    
-                Dans celle-ci, il est attaché et parle avec une psychologue. Il lui explique qu'elle ne connait pas ce qu'est vraiment une mauvaise journée et explique des sensations. Pendant la discussion, le joueur incarne le Joker qui se bat contre des hommes. Il est ensuite habillé en Red Hood dans une usine chimique. Dans la discussion avec la psychologue, il parle d'une rencontre spéciale, sans doute Batman. Dans l'usine, Le Joker est projeté dans une cuve d'acide, faisant de sa couleur de peau, un blanc anormal. Elle lui demande s'il connait cette personne, ce à quoi il indique ne pas connaitre sa véritable identité. La psychologue se présente et dit s'appeler Harleen Quinzel. Il lui demande alors si ses amis l'appelle Harley mais n'ayant pas beaucoup d'amis, ce n'est pas le cas. Le Joker lui dit que maintenant elle en a un. Il semble évident qu'elle tombe amoureuse de lui mais que ce dernier la manipule.
-    
-                Finalement, il est libre lors de l'émeute de la prison de Blackgate, menace Gordon, tire sur Joseph et se prend une raclée par Batman. Il est remis en prison. </p>          
+                 <h2 class=" text-4xl pb-4 uppercase font-bold">the joker</h2>
+                <ul>
+                    <li class="text-lg capitalize border-b-2 my-4 pb-2">nom complet : <br>
+                        <span class="text-xl uppercase font-bold">inconnu</span>
+                    </li>
+                    <li class="text-lg capitalize border-b-2 my-4 pb-2">surnom : <br>
+                        <span class="text-xl uppercase font-bold">Mr J</span>
+                    </li>
+                    <li class="text-lg capitalize border-b-2 my-4 pb-2">nom secret : <br>
+                        <span class="text-xl uppercase font-bold">red hood</span>
+                    </li>
+                    <li class="text-lg capitalize border-b-2 my-4 pb-2">activité : <br>
+                        <span class="text-xl uppercase font-bold">super vilain</span>
+                    </li>
+                    <li class="text-lg capitalize border-b-2 my-4 pb-2">allié : <br>
+                        <span class="text-xl uppercase font-bold">harley queen</span>
+                    </li>
+                    <li class="text-lg capitalize border-b-2 my-4 pb-2">ennemi : <br>
+                        <span class="text-xl uppercase font-bold">batman</span>
+                    </li>
+                    <li class="text-lg capitalize border-b-2 my-4 pb-2">famille : <br>
+                        <span class="text-xl uppercase font-bold">inconnu</span>
+                    </li>
+                </ul>
+                <p class="py-2 text-justify text-lg">Lors du soir de Noël, lorsque le Joker déguisé en Black Mask tente de déclencher une émeute, Batman entre dans la prison pour l'arrêter. Il tue le commissaire Gillian B. Loeb et quitte la prison.</p>          
             </div>
         </div> 
         <div class="pt-36 pb-10">
@@ -28,6 +45,12 @@
 </template>
 
 <script setup>
+
+    const jokers = [
+        { name: "inconnu", surname: "Mr J", secretID: "red hood", activité: "super criminel",
+            allié: "harley queen", ennemi:"batman", famille: "inconnu"
+        }
+    ]
 
 </script>
 
